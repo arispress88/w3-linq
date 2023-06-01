885a46ef6d5da6dc889c2c5a6901eee334b87f37
Arnold Rispress <arnold.rispress@gmail.com>
2023-06-01 15:31:09 -0400

    exercise 4 complete

---
+++ NumFreq/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] numbers = { 5, 9, 1, 5, 5, 9};
+
+        var numberFrequency = numbers.GroupBy(num => num)
+                                    .Select(group => new {Number = group.Key, Frequency = group.Count() });
+        
+        Console.WriteLine("The number and the Frequency are:");
+
+        foreach (var item in numberFrequency)
+        {
+            Console.WriteLine($"Number {item.Number} appears {item.Frequency}");
+        }
+    }
+}
