fa350454214762e060a7731e1292ace2a53317fe
Arnold Rispress <arnold.rispress@gmail.com>
2023-06-01 15:16:15 -0400

    linq exercise 3 completed

---
+++ SqRoot/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] numbers = { 9, 8, 6, 5};
+
+        var result = numbers.Select(num => new { Number = num, SqrNo = num * num });
+
+        foreach (var item in result)
+        {
+            Console.WriteLine($"{{Number = {item.Number}, SqrNo = {item.SqrNo}}}");
+        }
+    }
+}
