977f90c37279dba5073dfa466c68da7ba13d4a37
Arnold Rispress <arnold.rispress@gmail.com>
2023-06-02 11:18:39 -0400

    exercise 7 complete

---
+++ NumMulti/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = {5, 1, 9, 2, 3, 7, 4, 5, 6, 8, 7, 6, 3, 4, 5, 2};
+
+        var result = numbers
+            .GroupBy(n => n)
+            .Select(g => new
+            {
+                Number = g.Key,
+                Frequency = g.Count(),
+                Multiplication = g.Key * g.Count()
+            });
+
+        Console.WriteLine("Number | Frequency | Multiplication");
+        foreach (var item in result)
+        {
+            Console.WriteLine($"{item.Number, -7} | {item.Frequency, -9} | {item.Multiplication}");
+        }
+
+        Console.ReadLine();
+    }
+}
