1df2a3fe5d107f60adff25f9ceed986743e816da
Arnold Rispress <arnold.rispress@gmail.com>
2023-06-02 10:20:35 -0400

    exercise 6 complete

---
+++ WeekDays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string[] daysOfWeek = Enum.GetValues(typeof(DayOfWeek))
+                                .Cast<DayOfWeek>()
+                                .Select(d => d.ToString())
+                                .ToArray();
+
+        Console.WriteLine("Days of the Week:");
+        foreach (string day in daysOfWeek)
+        {
+            Console.WriteLine(day);
+        }
+
+        Console.ReadLine();
+    }
+}
